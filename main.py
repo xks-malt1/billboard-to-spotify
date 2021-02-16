71338ba3fcaace5c64a7291380690316d191466c
vault7 <azirij@gmail.com>
2021-02-16 20:17:45 +0100

    only changes with token refreshing

--- main.py
+++ main.py
@@ -22,5 +22,4 @@ artists = soup.find_all(class_="chart-element__information__artist text--truncat
 artists = [artist.string for artist in artists]
 print(song_titles[0])
 print(artists[0])
-with open("sample.json", "w") as file:
-    json.dump(spotify_app.search_track(song_titles[0], artists[0]), file, indent=4)
+print(spotify_app.search_track(song_titles[0], artists[0]))
--- spotify.py
+++ spotify.py
@@ -2,16 +2,17 @@ import os
 from dotenv import load_dotenv
 import requests
 import base64
-import json
 
 load_dotenv()
 
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REFRESH_TOKEN = os.getenv("REFRESH_TOKEN")
 HEADERS_BASIC = {"Authorization": f"Basic {base64.b64encode((CLIENT_ID +':'+CLIENT_SECRET).encode()).decode()}"}
 
-with open("token.json") as file:
-    spotify_bearer_token = json.load(file)["access_token"]
+
+with open("token.txt") as file:
+    spotify_bearer_token = file.read()
 
 API_TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token"
 API_ENDPOINT = "https://api.spotify.com/v1/me"
@@ -28,18 +29,14 @@ class SpotifyApp:
         response = requests.get(API_ENDPOINT, headers=self.headers_token)
 
         if response.status_code == 401:
-            with open("token.json") as file:
-                token_data = json.load(file)
-                refresh_token = token_data["refresh_token"]
             data = {
                 "grant_type": "refresh_token",
-                "refresh_token": refresh_token
+                "refresh_token": REFRESH_TOKEN
             }
             response = requests.post(API_TOKEN_ENDPOINT,data=data, headers=HEADERS_BASIC)
-            token_data["access_token"] = response.json()["access_token"]
-            self.headers_token = {"Authorization": f"Bearer {token_data['access_token']}"}
-            with open("token.json", "w") as file:
-                json.dump(token_data, file, indent=4)
+            self.headers_token = {"Authorization": f"Bearer {response.json()['access_token']}"}
+            with open("token.txt", "w") as file:
+                file.write(response.json()["access_token"])
 
             response = requests.get(API_ENDPOINT, headers=self.headers_token)
             self.id = response.json()["id"]
@@ -47,11 +44,6 @@ class SpotifyApp:
             self.id = response.json()["id"]
 
     def search_track(self, track_name, artist):
-        parameters = {
-            "q": f"track:{track_name}+artist:{artist}",
-            "type": "track",
-
-        }
-
-        response = requests.get(SEARCH_API, params=parameters, headers=self.headers_token)
-        return response.json()
+        
+        response = requests.get(f"{SEARCH_API}?q=track:{track_name}+artist:{artist}&type=track&limit=1", headers=self.headers_token)
+        return response.json()["tracks"]["items"][0]["uri"]
